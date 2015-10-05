496e14633ec40c17771d83ef4708806e884d4d54
yeshdev1 <yeshdev@umich.edu>
2015-10-05 19:55:01 -0400

    project setup

---
+++ Hexoplex/Hexoplex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hexoplex
+//
+//  Created by Yeshwanth Devabhaktuni on 10/5/15.
+//  Copyright (c) 2015 Yeshwanth Devabhaktuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
