15b86e941549d6199afa756bd847cddbc0e1609f
cheningD <cdduker@gmail.com>
2015-11-01 13:43:54 -0500

    setting up GaugeKit

---
+++ Hexoplex/Hexoplex/HexoskinInterface.swift
@@ -0,0 +1,9 @@
+//
+//  HexoskinInterface.swift
+//  Hexoplex
+//
+//  Created by Chening Duker on 11/1/15.
+//  Copyright © 2015 Yeshwanth Devabhaktuni. All rights reserved.
+//
+
+import Foundation
